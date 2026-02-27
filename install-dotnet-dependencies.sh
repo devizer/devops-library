ca79138ae1d1d3da46eb1b174dc1c075a10960ba
devizer <devizer@users.github.io>
2026-02-28 01:04:16 +0200

    openssl release

--- install-dotnet-dependencies.sh
+++ install-dotnet-dependencies.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Here is one line installer 
 # url=https://devizer.github.io/devops-library/install-dotnet.sh; (wget -q -nv --no-check-certificate -O - $url 2>/dev/null || curl -ksSL $url) | UPDATE_REPOS=true bash -e && echo "Successfully installed .NET Core Dependencies"
-set +e; set -o pipefial;
+set +e; set -o pipefail;
 
 sudo="$(command -v sudo || true)"; [[ "$(uname -s)" == "MSYS"* || ""$(uname -s)"" == "MINGW"* ]] && sudo="";
 
